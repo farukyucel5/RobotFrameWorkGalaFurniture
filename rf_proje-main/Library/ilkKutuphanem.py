96721d96b1ce9dc6d13b1c63aa9a8f6b18bcd842
Faruk Yucel <farukyucel5@gmail.com>
2023-01-05 02:15:50 +0300

    updating

--- rf_proje-main/Library/ilkKutuphanem.py
+++ rf_proje-main/Library/ilkKutuphanem.py
@@ -6,17 +6,3 @@ def topla(sayi1, sayi2) -> int:
 
 def rastgeleSayi(baslangic, bitis) -> int:
     return random.randint(int(baslangic), int(bitis))
-
-
-def isElementVisible(self, locator):
-    """Returns element visibility identified by ``locator``.
-
-    Arguments:
-    - ``locator``: The locator to find requested element. Key attributes for
-                    arbitrary elements are ``id`` and ``name``. See `introduction` for
-                    details about locating elements.
-
-    Examples:
-    | Is Element Visible | css=div.class |
-    """
-    return self.is_visible(locator)
